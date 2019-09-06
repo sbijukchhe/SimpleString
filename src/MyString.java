21726ced1a2e6b7fd0d3278ad16de2c8520c86da
GBTC441013UR <GBTC441013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-06 15:06:25 -0400

    Simple String

---
+++ src/MyString.java
@@ -0,0 +1,17 @@
+/*
+Write a Java program that assigns your name to a variable and prints the variable to the console.
+ */
+
+public class MyString {
+
+    public static void main(String[] args) {
+
+        String name;
+        //name = new String("Surendra K Bijukchhe");
+
+        // alternative way to create String object
+        name = "Surendra Bijukchhe";
+        System.out.println(name);
+
+    }
+}
